7696b4d260bbd90655a8c13038e65b5a595365c6
oysi-desktop <oysi93@gmail.com>
2024-03-17 03:26:34 +0100

    changes

--- init-script.sh
+++ init-script.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+# init linux
 apt update
 apt full-upgrade -y
 apt dist-upgrade -y
-apt install -y sudo git docker docker-compose
+apt install -y sudo git docker docker-compose ufw
 adduser oysi sudo
+
+# init ufw
+apt install -y ufw
+ufw limit 22/tcp
+ufw allow 80/tcp
+ufw allow 443/tcp
+ufw default deny incoming
+ufw default allow outgoing
+ufw enable
