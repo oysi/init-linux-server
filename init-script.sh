397aae827456949b6e7baa53c8a4ebc5bd801baf
oysi-desktop <oysi93@gmail.com>
2024-03-17 03:28:55 +0100

    changes

--- init-script.sh
+++ init-script.sh
@@ -4,14 +4,13 @@
 apt update
 apt full-upgrade -y
 apt dist-upgrade -y
-apt install -y sudo git docker docker-compose ufw
+apt install -y sudo git docker docker-compose ufw neofetch
 adduser oysi sudo
 
 # init ufw
-apt install -y ufw
 ufw limit 22/tcp
 ufw allow 80/tcp
 ufw allow 443/tcp
 ufw default deny incoming
 ufw default allow outgoing
-ufw enable
+ufw -y enable
